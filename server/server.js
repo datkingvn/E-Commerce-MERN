78079bca9371f7f67c9d65d4073466b95b6ec105
Mạnh Đạt <tranhuudats1@gmail.com>
2023-09-12 14:12:50 +0700

    Base Project

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+const app = express()
+dotenv.config()
+const PORT = process.env.PORT || 8888
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.use('/', (req, res) => {
+    res.send('Sever ONN')
+})
+
+app.listen(PORT, () => {
+    console.log('Server Running ON Port: ' + PORT)
+})

884829eae43f787c5743d393bdb16f29acd3b0be
Mạnh Đạt <tranhuudats1@gmail.com>
2023-09-15 15:25:02 +0700

    Logic Access Token

--- server/middlewares/jwt.js
+++ server/middlewares/jwt.js
@@ -1,20 +1,12 @@
 const jwt = require('jsonwebtoken')
 
-
 // jwt.sign(payload, secretOrPrivateKey, options);
 const generateAccessToken = (uid, role) => {
-    // Tạo một JSON Web Token (JWT) mới
-    return jwt.sign({
-        _id: uid,
-        role: role
-    }, process.env.JWT_SECRET, {expiresIn: '1d'});
+    return jwt.sign({_id: uid, role: role}, process.env.JWT_SECRET, {expiresIn: '1d'});
 }
 
 const generateRefreshToken = (uid) => {
-    // Tạo một JSON Web Token (JWT) mới
-    return jwt.sign({
-        _id: uid,
-    }, process.env.JWT_SECRET, {expiresIn: '7d'});
+    return jwt.sign({_id: uid,}, process.env.JWT_SECRET, {expiresIn: '7d'});
 }
 
 module.exports = {

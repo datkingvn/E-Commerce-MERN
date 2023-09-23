555aaaac5d32cb4b766eed8b7ffa7a36141fd23a
Mạnh Đạt <tranhuudats1@gmail.com>
2023-09-24 04:48:02 +0700

    Fix Frontend (Header & Nav)

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -18,7 +18,7 @@ const Sidebar = () => {
     return (
         <div className='flex flex-col border'>
             {categories?.map(el => (
-                <NavLink key={createSlug(el)} to={createSlug(el.title)}
+                <NavLink key={createSlug(el.title)} to={createSlug(el.title)}
                          className={({isActive}) => isActive ? 'bg-main text-white px-5 pt-[15px] pb-[14px] text-sm hover:text-main' : 'px-5 pt-[15px] pb-[14px] text-sm hover:text-main'}>
                     {el.title}
                 </NavLink>
